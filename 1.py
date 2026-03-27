943972698e2935a6f36674735d855778a193d56b
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-03-27 21:17:30 +0530

    python problems

---
+++ 1.py
@@ -0,0 +1,15 @@
+# Write a code using a while loop that adds all odd numbers from 1 to 100 (inclusive).
+
+
+n=1
+
+while n<=100:
+    print(n)
+    n+=2
+
+#Using for loop
+
+for i in range(1,101,2):
+    print(i)
+
+
